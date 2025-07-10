f1ad35150f4a1f9c4819b60ee809e0568d673aeb
amags727 <86733530+amags727@users.noreply.github.com>
2025-07-10 17:02:11 -0400

    updated c1

--- 6_matlab_simulation_work/c_deterministic_pe_closed_econ/c1_HJB.m
+++ 6_matlab_simulation_work/c_deterministic_pe_closed_econ/c1_HJB.m
@@ -1,40 +1,41 @@
-clear all; close all; 
+clear all; close all; clc;
 addpath(genpath('c_helper_functions'))
 
-% production parameters 
-w = 1; % wage
-phi_g = 1; % goods productivity
-gamma = 4;     % CES parameter (from BEJK)
-gamma_tilde = gamma/(gamma-1);
-
-% data parameters 
-phi_d = 10; % data productivity 
+%key parameters
+I = 100;
+w = 1; % data worker wage
+phi_d = 1; % data productivity 
 alpha_1 = .5;  % cobb douglas coefficient on data labor 
 alpha_2 = .5; % cobb douglas coefficient on raw data 
-sigma_a = 1.1; % sd of noise term 
-sqrt_Q = 1.1; % sd of random component of z
-Q = sqrt_Q^2;
+top_bottom_quality_ratio = 2; % how much data helps improve quality 
+x_scale_factor = 20; % what is underlying level of demand 
+phi_g = 1;
+Q = 1.1.^2; % variance of random component of z
 theta = .9; % mean reversion parameter of z (closer to one faster mean reversion)
+
+% Other Data parameters
+sigma_a = 1.1; % sd of noise term 
+
+% production parameters 
+gamma = 4;     % CES parameter (from BEJK)
+gamma_tilde = gamma/(gamma-1);
+x_bar = x_scale_factor*phi_g^gamma; % base demand 
+pi_bar = x_bar*w*phi_g^-1*(gamma-1)^-1; % base profits 
 
 % Simulation Parameters
 rho = 0.05; %discount rate
 Delta = 1000; % 1/Delta = time_step
 crit = 10^(-6);
-maxit = 500;
+maxit = 50;
 
 % Define the state space 
-I = 20;
-Sigma_ub = sqrt_Q^2 /(2*theta); % Sigma_ub is the value such that drift = 0 when the firm doesn't participate in market 
+Sigma_ub = Q /(2*theta); % Sigma_ub is the value such that drift = 0 when the firm doesn't participate in market 
 Sigma_lb = 1e-2;
 Sigma = linspace(Sigma_lb,Sigma_ub, I)';
 d_Sigma = Sigma(2) - Sigma(1);
 
 % Define Expected Quality (A_tilde) and related values 
-penalty =  Sigma + sigma_a^2;
-A_bar = max(penalty) + (max(penalty)-min(penalty));
-A_tilde = A_bar - penalty;
-x_bar = 1/min(A_tilde); % base demand 
-pi_bar = x_bar*w*phi_g^-1*(gamma-1)^-1; % base profits 
+A_tilde = ch1_gen_A_tilde(top_bottom_quality_ratio,Sigma, sigma_a);
 E_x = x_bar*A_tilde;
 E_pi = pi_bar*A_tilde;
 xi = alpha_1*phi_d*E_x.^alpha_2;
@@ -43,13 +44,11 @@ xi = alpha_1*phi_d*E_x.^alpha_2;
 % of no data consumption forever 
 R_0 = sigma_a^(-2);
 Sigma_0 = (-theta + sqrt(theta^2 + Q*R_0))/R_0;
-[~, idx] = min(abs(Sigma - Sigma_0));
-V_0 = repmat(E_pi(idx) / rho, I,1);
+[~, idx_no_data_ss] = min(abs(Sigma - Sigma_0));
+V_0 = repmat(E_pi(idx_no_data_ss) / rho, I,1);
 v= V_0;
 
 for n=1:maxit
-    disp(n)
-
     V=v;
     % Define forward/ backward difference  
     dv_f = [V(2:I)-V(1:I-1); 0] / d_Sigma;
@@ -61,9 +60,9 @@ for n=1:maxit
     %carry out upwind procedure 
     %noting that Sigma_dot is decreasing in dv_Sigma
     dv_min = min(dv_b,dv_f); dv_max =  max(dv_b,dv_f);
-    optim_min = c1_optim_calc(dv_min,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
+    optim_min = ch2_optim_calc(dv_min,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
     phi_d, sigma_a,Q, w, theta);
-    optim_max = c1_optim_calc(dv_max,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
+    optim_max = ch2_optim_calc(dv_max,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
     phi_d, sigma_a,Q, w, theta);
 
     Ib = false(I,1); If = Ib; I_final = If;
@@ -74,7 +73,7 @@ for n=1:maxit
         Ib.*dv_b + If.*dv_f +... 
         (~I_final & optim_min.ham > optim_max.ham).*dv_min + ...
         (~I_final & optim_min.ham <= optim_max.ham).*dv_max;
-    optimal = c1_optim_calc(dv_final,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
+    optimal = ch2_optim_calc(dv_final,Sigma,xi, E_x, E_pi, alpha_1, alpha_2,...
     phi_d, sigma_a,Q, w, theta);
     
     %CONSTRUCT TRANSITION MATRIX 
@@ -107,10 +106,44 @@ for n=1:maxit
     
     dist(n) = max(max(abs(Vchange)));
     if dist(n)<crit
-            disp('Value Function Converged, Iteration = ')
-            disp(n)
+        %fprintf('Value Function Converged, Iteration = %g\n',n)
             break
+    end
+    if n== maxit
+        disp('failed to converge')
     end
 end
 
-optimal.L
+ss_indx =  find(optimal.drift < 0, 1);
+L_at_SS = optimal.L(ss_indx);
+alpha_determinant = -(1./w) .* xi .* dv_final .* Sigma.^2 /alpha_1;
+output_ratio_ss_vs_no_data = round(100*(E_x(ss_indx) / E_x(idx_no_data_ss)- 1));
+fprintf('percent output increase from no data: (%g)\n',output_ratio_ss_vs_no_data);
+fprintf('S.S L value/position: (%g,%g)\n',round(L_at_SS,3), ss_indx);
+%fprintf('S.S alpha determ fac. / dv: (%g,%g)\n',round(alpha_determinant(ss_indx),2), round(dv_final(ss_indx),2));
+
+
+%% Simulate firm trajectory starting at Sigma_ub
+
+T = 1000;                    % number of periods
+Sigma_path = zeros(T, 1);
+data_use_path = zeros(T, 1);
+profit_path = zeros(T, 1);
+output_path = zeros(T, 1);
+
+Sigma_path(1) = Sigma_ub;  % initial condition
+
+% Derivative of value function (finite difference)
+for t = 1:T
+    Sigma_t = Sigma_path(t);
+    data_use_path(t) =  interp1(Sigma, optimal.L, Sigma_t, 'linear', 'extrap');
+    output_path(t) = interp1(Sigma, E_x, Sigma_t, 'linear', 'extrap');
+    profit_path(t) =  interp1(Sigma, E_pi, Sigma_t, 'linear', 'extrap');
+    drift_t =  interp1(Sigma, optimal.drift, Sigma_t, 'linear', 'extrap');
+    if (t ~= T)
+        Sigma_path(t+1) = Sigma_t + drift_t*1/Delta;
+    end 
+end
+
+%% Plot the results
+%plot(1:T, data_use_path)
