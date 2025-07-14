bbf942e38308bf5b36a47f01fb6597d754acf182
amags727 <86733530+amags727@users.noreply.github.com>
2025-07-14 09:28:55 -0400

    update d to get it working

--- 6_matlab_simulation_work/d_deterministic_open_econ/d_helper_functions/dh3_set_v0_fc_ec.m
+++ 6_matlab_simulation_work/d_deterministic_open_econ/d_helper_functions/dh3_set_v0_fc_ec.m
@@ -1,5 +1,5 @@
-function [v0,fc] = fh4_set_v0_and_fc(Sigma_mat,Sigma, D,Q,sigma_a,...
-    pi_bar, A_tilde,networks, fixed_cost_scaling,rho)
+function [v0,fc,ec] = dh3_set_v0_fc_ec(Sigma_mat,Sigma, D,Q,sigma_a,...
+    pi_bar, A_tilde,networks, fixed_cost_scaling,ec_multiplier, rho)
 
 [~,num_mkts,len_Sigma, num_networks] = size(Sigma_mat);
 v0 = zeros(len_Sigma, size(networks,1));
@@ -18,6 +18,7 @@ for i = 0:num_networks
     if i == 0
         fc = pi_bar(1)*A_tilde(init_index,1);
         fc = [fc, repmat(fc*fixed_cost_scaling,1,num_mkts-1)];
+        ec = fc*ec_multiplier;
     else
         v0(:,i) = sum((pi_bar.*A_tilde(init_index,:) - fc).*networks(i,:))/ rho;
     end
--- 6_matlab_simulation_work/d_deterministic_open_econ/d_helper_functions/dh8_update_V.m
+++ 6_matlab_simulation_work/d_deterministic_open_econ/d_helper_functions/dh8_update_V.m
@@ -1,11 +1,11 @@
-function V = dh8_update_V(LCP_yn,profit,v, A_matrix,A_matrix_cells, rho, Delta, networks, ec, rev_ec)
+function [V,z,best_alt] = dh8_update_V(LCP_yn,profit,v, A_matrix,A_matrix_cells, rho, Delta, networks, ec, rev_ec)
 %===SETUP ====
 [len_Sigma, num_networks] = size(v);
 
    
 % If we're just doing value func iteration 
 if ~LCP_yn
-    V = zeros(size(v));
+    V = zeros(size(v)); z = 0; best_alt = 0;
     for network = 1:num_networks
         B = (rho + 1/Delta)*speye(len_Sigma) - A_matrix_cells{network};
         b = profit(:,network) + v(:,network)./Delta;
@@ -34,9 +34,10 @@ else
     z = LCP(B,q,l,u,z,0);
     LCP_error = max(abs(z.*(B*z + q)));
     if LCP_error > 10^(-5)
-        fprintf('LCP not solved');
+        fprintf('LCP not solved: error = %g\n', LCP_error);
     end
     % update the value function
     V= reshape(z+ vstar_stacked, [], num_networks);
+    z = reshape(z, [], num_networks);
 end
 end
